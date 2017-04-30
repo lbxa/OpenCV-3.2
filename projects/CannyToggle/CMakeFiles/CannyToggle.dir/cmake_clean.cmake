315a40814959de7d524a1ea85ff188b72ac9a44c
Lucas Barbosa <lucas@localhost.localdomain>
2017-04-30 21:06:44 +1000

    New ToggleCanny interface software

---
+++ projects/CannyToggle/CMakeFiles/CannyToggle.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CannyToggle.dir/CannyToggle.cpp.o"
+  "CannyToggle.pdb"
+  "CannyToggle"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CannyToggle.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
